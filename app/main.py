9df2fd5491a0ee5e68204b7a0630a42ff205e702
CarlosLara25 <carlos_25155@hotmail.com>
2026-08-19 11:47:21 -0600

    Add schemas and predict endpoint

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 
+from app.inference.schemas import PredictionRequest, ResponseSchema
+
 from app.inference.loader import load_inference_artifacts
+
+from uuid import uuid4
+from datetime import datetime, timezone
+
+import pandas as pd
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -24,3 +31,33 @@ app = FastAPI(
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+@app.post("/predict", response_model=ResponseSchema)
+def prediction(data_request: PredictionRequest):
+    model = app.state.model
+    preprocessor = app.state.preprocessor
+    encoder = app.state.encoder
+    metadata = app.state.metadata
+
+    # Request → DataFrame
+    data = data_request.model_dump()
+    X = pd.DataFrame([data])
+
+    X_transformed = preprocessor.transform(X)
+
+    y_pred = model.predict(X_transformed)
+    probabilities = model.predict_proba(X_transformed)
+
+    prediction = encoder.inverse_transform(y_pred)[0]
+
+    confidence = float(probabilities[0][y_pred[0]])
+
+    response = {
+        "request_id":str(uuid4()),
+        "prediction":prediction,
+        "confidence":confidence,
+        "model_version":metadata["version"],
+        "timestamp":datetime.now(timezone.utc),
+        }
+    
+    return response
